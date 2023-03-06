849418a1a1839fe1a6a3512641df30621edd754a
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2023-03-06 14:30:47 +0200

    exercise 01

---
+++ homework/08/01/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+      System.out.println(currentTime);
+  }
+}
